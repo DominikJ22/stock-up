81b438d810884a49b3eeaef42859ad9d400dad4c
DominikJ22 <dominikjo22@gmail.com>
2023-01-12 22:49:51 -0500

    showing products from database

--- my-ecommerce/pages/index.js
+++ my-ecommerce/pages/index.js
@@ -1,12 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import {useEffect} from "react";
+import Product from "../components/Product";
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({products}) {
+  const [phrase,setPhrase] = useState('');
+  
+  const categoriesNames = [...new Set(products.map(p => p.category))]
+
   return (
-    <div></div>
+    <div className="p-5">
+      <div>
+        {categoriesNames.map(categoryName => (
+          <div key={categoryName}>
+          <h2 className="text-2xl capitaliza">{categoryName}</h2>
+          {productsInfo.filter(p => p.category === categoryName.map ( <Product>
+            ))}
+          </div>
+        ))}
+        <div className="py-4">
+        </div>
+      </div>
+    </div>
   )
 }
